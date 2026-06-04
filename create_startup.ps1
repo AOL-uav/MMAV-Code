c7ee1c5121f1f4ea66e060573127bb0ef49e2fa4
Kai Keller <kxkeller01@gmail.com@example.com>
2026-06-03 20:20:14 -0400

    Hide sync scripts from repository tracking

--- create_startup.ps1
+++
@@ -1,7 +0,0 @@
-$startupFolder = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup')
-$shortcutPath = [System.IO.Path]::Combine($startupFolder, 'MAV_Sync.lnk')
-$ws = New-Object -ComObject WScript.Shell
-$s = $ws.CreateShortcut($shortcutPath)
-$s.TargetPath = 'wscript.exe'
-$s.Arguments = '"C:\Users\Kai\OneDrive - purdue.edu\Purdue\AOL\MAV-2026\Controls_PCB\silent_launch.vbs"'
-$s.Save()
--- silent_launch.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run "powershell.exe -ExecutionPolicy Bypass -File ""C:\Users\Kai\OneDrive - purdue.edu\Purdue\AOL\MAV-2026\Controls_PCB\win_sync.ps1""", 0, False
