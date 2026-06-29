0f9498dc209fffc2d8f7b0c918a4038a8c64d4dc
Kai Keller <kxkeller01@example.com>
2026-06-28 20:40:59 -0400

    Auto-save (Fedora): 2026-06-28 20:40

--- test/test.ino
+++ test/test.ino
@@ -1,24 +1,54 @@
+#include <TinyGPSPlus.h>
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+// The GPS is connected to Serial1
+// Connect GPS TX to Nano RP2040 RX (Pin 0)
+// Connect GPS RX to Nano RP2040 TX (Pin 1)
+// Connect GPS VCC to 3.3V or 5V (depending on your module)
+// Connect GPS GND to GND
+
 void setup() {
   Serial.begin(115200);
-  Serial1.begin(115200); // Try 115200 baud  
+  
+  // We found out the GPS module communicates at 115200 baud!
+  Serial1.begin(115200); 
+  
+  // Wait for serial monitor to open (optional, but good for debugging)
   while (!Serial) {
     delay(10);
   }
   
-  Serial.println(F("Raw GPS Echo Test Started"));
-  Serial.println(F("Waiting for NMEA sentences..."));
+  Serial.println(F("Nano RP2040 GPS Test"));
+  Serial.println(F("Waiting for GPS data..."));
 }
 
 void loop() {
-  // Echo everything from GPS to the Serial Monitor
+  // Read incoming characters from the GPS
   while (Serial1.available() > 0) {
-    Serial.write(Serial1.read());
+    gps.encode(Serial1.read());
+  }
+
+  // Print location when it is updated
+  if (gps.location.isUpdated()) {
+    Serial.print(F("Location: "));
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(", "));
+    Serial.println(gps.location.lng(), 6);
+    
+    Serial.print(F("Satellites: "));
+    Serial.println(gps.satellites.value());
+    
+    Serial.print(F("Altitude: "));
+    Serial.print(gps.altitude.meters());
+    Serial.println(F(" m"));
+    Serial.println();
   }
   
-  // Also print a heartbeat every 5 seconds so we know the board isn't frozen
-  static unsigned long lastPrint = 0;
-  if (millis() - lastPrint > 5000) {
-    Serial.println(F("[Heartbeat] Still waiting..."));
-    lastPrint = millis();
+  // Warning if no data is coming in
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println(F("No GPS data received: check wiring!"));
+    delay(2000);
   }
 }
