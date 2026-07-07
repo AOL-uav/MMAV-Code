ec10223dbbfbbbe49058f58b5ca23fc2e2c8fddf
Kai Keller <kxkeller01@example.com>
2026-07-07 13:02:13 -0400

    Auto-save (Fedora): 2026-07-07 13:02

--- sweep stuff/sweep_aero_logger/sweep_aero_logger.ino
+++ sweep stuff/sweep_aero_logger/sweep_aero_logger.ino
@@ -72,7 +72,7 @@ static const int PIN_MOUNT     = A0;
 static const int PIN_UNFOLD    = A1;
 static const int PIN_FOLD      = A2;
 
-static const int SWEEP_UNFOLDED_US = 2500;
+static const int SWEEP_UNFOLDED_US = 2450;
 static const int SWEEP_FOLDED_US   = 500;
 static const int SWEEP_MOUNT_US    = 700;
 
