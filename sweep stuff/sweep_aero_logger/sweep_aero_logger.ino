7b9f2780da25379c94932a80bccb924ae5fbe059
Kai Keller <kxkeller01@example.com>
2026-07-07 13:24:56 -0400

    Auto-save (Fedora): 2026-07-07 13:24

--- sweep stuff/sweep_aero_logger/sweep_aero_logger.ino
+++ sweep stuff/sweep_aero_logger/sweep_aero_logger.ino
@@ -1981,46 +1981,7 @@ void loop() {
     currentAoaRightUs = AOA_RIGHT_FOLDED_US;
     
     slowSweepTo(SWEEP_FOLDED_US);
-    
-    // Wait for 40 seconds (2000 steps of 20ms) non-blocking for logger
-    for (int i = 0; i < 2000; i++) {
-        uint32_t stepStartMs = millis();
-        while (millis() - stepStartMs < 20) {
-            pollGps();
-            const uint32_t nowUs2 = micros();
-            if ((int32_t)(nowUs2 - nextControlUs) >= 0) {
-                nextControlUs += CONTROL_DT_US;
-                const uint32_t controlDtUs2 = nowUs2 - lastControlUs;
-                lastControlUs = nowUs2;
-                float dt2 = controlDtUs2 * 1.0e-6f;
-                if (dt2 <= 0.0f || dt2 > 0.1f) dt2 = DT_NOMINAL;
-                const uint32_t controlStartUs2 = micros();
-                if (!imuFaultLocked && readImu(imu)) {
-                    const uint32_t esekfStartUs2 = micros();
-                    predictEsekf(imu, dt2);
-                    updateEsekfWithAccelerometer(imu);
-                    updateEsekfWithGps();
-                    const uint32_t esekfUs2 = micros() - esekfStartUs2;
-                    eulerFromQuaternion(esekf.q, rollRad, pitchRad, yawRad);
-                    const uint32_t controlExecUs2 = micros() - controlStartUs2;
-                    maybeEnqueueLog(controlDtUs2, controlExecUs2, esekfUs2);
-                }
-            }
-        }
-    }
-    
-    // Automatically unfold after 40 seconds
-    slowSweepTo(SWEEP_UNFOLDED_US);
-    
-    servoLeft.writeMicroseconds(AOA_LEFT_FLAT_US);
-    servoRight.writeMicroseconds(AOA_RIGHT_FLAT_US);
-    currentAoaLeftUs = AOA_LEFT_FLAT_US;
-    currentAoaRightUs = AOA_RIGHT_FLAT_US;
-    sweepMode = SWEEP_UNFOLDED;
-    
-    while(digitalRead(PIN_FOLD) == LOW) {
-      delay(20);
-    }
+    sweepMode = SWEEP_FOLDED;
     
   // 2. Check if Unfold is pressed
   } else if (digitalRead(PIN_UNFOLD) == LOW && sweepMode != SWEEP_UNFOLDED) {
