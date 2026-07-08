635f112b7cc78356e81b7357291a85f93cd237a0
Kai Keller <kxkeller01@example.com>
2026-07-07 20:34:21 -0400

    Auto-save (Fedora): 2026-07-07 20:34

--- test/rotational_mode/rotational_mode_test/rotational_mode_test.ino
+++ test/rotational_mode/rotational_mode_test/rotational_mode_test.ino
@@ -65,7 +65,7 @@
 
 // ========================= Edit each flight =========================
 
-static const char LOG_TAG[] = "7/07";
+static const char LOG_TAG[] = "707";
 
 
 // ========================= Sweep Constants =========================
