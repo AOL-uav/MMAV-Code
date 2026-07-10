73b9fd3399d68ab947056869f2b56bc60300c8b4
Kai Keller <kxkeller01@example.com>
2026-07-10 13:36:14 -0400

    Auto-save (Fedora): 2026-07-10 13:36

--- test/basic_sd/basic_sd.ino
+++ test/basic_sd/basic_sd.ino
@@ -2,8 +2,9 @@
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial && millis() < 3000);
-  
+}
+
+void loop() {
   Serial.println("\n--- Pin Mappings ---");
   Serial.print("D10: "); Serial.println(D10);
   Serial.print("D11: "); Serial.println(D11);
@@ -13,11 +14,9 @@ void setup() {
   Serial.print("MOSI: "); Serial.println(MOSI);
   Serial.print("SCK: "); Serial.println(SCK);
   Serial.print("SS: "); Serial.println(SS);
+  delay(2000);
 }
 
-void loop() {
-  delay(1000);
-}
 
 
 
