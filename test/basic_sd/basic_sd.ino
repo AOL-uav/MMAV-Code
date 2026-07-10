52a613f5188e5b2027e4c99b72443ee38785c57d
Kai Keller <kxkeller01@example.com>
2026-07-10 13:04:00 -0400

    Auto-save (Fedora): 2026-07-10 13:04

--- test/basic_sd/basic_sd.ino
+++ test/basic_sd/basic_sd.ino
@@ -127,92 +127,24 @@ void setup()
   Serial.println(BOARD_NAME);
   Serial.println(RP2040_SD_VERSION);
   
+void loop() {
   Serial.print("Initializing SD card with SS = ");  Serial.println(PIN_SD_SS);
   Serial.print("SCK = ");   Serial.println(PIN_SD_SCK);
   Serial.print("MOSI = ");  Serial.println(PIN_SD_MOSI);
   Serial.print("MISO = ");  Serial.println(PIN_SD_MISO);
 
-  // we'll use the initialization code from the utility libraries
-  // since we're just testing if the card is working!
   if (!card.init(SPI_HALF_SPEED, chipSelect)) 
   {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    while (1);
+    Serial.println("initialization failed.");
   } 
   else 
   {
     Serial.println("Wiring is correct and a card is present.");
+    if (!SD.begin(PIN_SD_SS)) {
+      Serial.println("SD.begin failed!");
+    } else {
+      Serial.println("SD.begin OK!");
+    }
   }
-
-  // print the type of card
-  Serial.println();
-  Serial.print("Card type:         ");
-  
-  switch (card.type()) 
-  {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-
-  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) 
-  {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    while (1);
-  }
-
-  Serial.print("Clusters:          ");
-  Serial.println(volume.clusterCount());
-  Serial.print("Blocks x Cluster:  ");
-  Serial.println(volume.blocksPerCluster());
-
-  Serial.print("Total Blocks:      ");
-  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
-  Serial.println();
-
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print("Volume type is:    FAT");
-  Serial.println(volume.fatType(), DEC);
-
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
-  Serial.print("Volume size (Kb):  ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mb):  ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Gb):  ");
-  Serial.println((float)volumesize / 1024.0);
-
-  if (!SD.begin(PIN_SD_SS)) 
-  {
-    Serial.println("Initialization failed!");
-    return;
-  }
-  
-  Serial.println("Initialization done.");
-
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-
-  root = SD.open("/");
-
-  printDirectory(root, 0);
-}
-
-void loop() 
-{
+  delay(2000);
 }
