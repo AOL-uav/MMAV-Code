7a82a4f6ef02849dc99699e501af249f63fc07ce
Kai Keller <kxkeller01@example.com>
2026-07-10 13:10:26 -0400

    Auto-save (Fedora): 2026-07-10 13:10

--- test/basic_sd/basic_sd.ino
+++ test/basic_sd/basic_sd.ino
@@ -1,38 +1,20 @@
+#include <Arduino.h>
 #include <SPI.h>
-#include "RP2040_SD.h"
+#include <SD.h>
 
-#define PIN_SD_MOSI       PIN_SPI_MOSI
-#define PIN_SD_MISO       PIN_SPI_MISO
-#define PIN_SD_SCK        PIN_SPI_SCK
-
-const int chipSelect = PIN_SPI_SS;
-
-Sd2Card card;
-SdVolume volume;
-SdFile root;
+const int chipSelect = 10;
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  Serial.print("Initializing SD card with SS = ");  Serial.println(PIN_SPI_SS);
-  Serial.print("SCK = ");   Serial.println(PIN_SPI_SCK);
-  Serial.print("MOSI = ");  Serial.println(PIN_SPI_MOSI);
-  Serial.print("MISO = ");  Serial.println(PIN_SPI_MISO);
+  Serial.println("Initializing SD card...");
 
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) 
-  {
-    Serial.println("initialization failed.");
-  } 
-  else 
-  {
-    Serial.println("Wiring is correct and a card is present.");
-    if (!SD.begin(PIN_SPI_SS)) {
-      Serial.println("SD.begin failed!");
-    } else {
-      Serial.println("SD.begin OK!");
-    }
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+  } else {
+    Serial.println("initialization done.");
   }
   delay(2000);
 }
