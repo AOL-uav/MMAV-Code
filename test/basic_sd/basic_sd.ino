3d79f5b0811b7ec5a87d0a2aba4cafa576fc3c86
Kai Keller <kxkeller01@example.com>
2026-07-10 13:35:41 -0400

    Auto-save (Fedora): 2026-07-10 13:35

--- test/basic_sd/basic_sd.ino
+++ test/basic_sd/basic_sd.ino
@@ -1,47 +1,24 @@
-#include <SPI.h>
-#include "SdFat.h"
-
-#define SD_CS_PIN 5
-#define SD_MOSI_PIN 7
-#define SD_MISO_PIN 4
-#define SD_SCK_PIN 6
-
-SdFat sd;
-bool initSuccess = false;
-bool tried = false;
+#include <Arduino.h>
 
 void setup() {
   Serial.begin(115200);
+  while (!Serial && millis() < 3000);
+  
+  Serial.println("\n--- Pin Mappings ---");
+  Serial.print("D10: "); Serial.println(D10);
+  Serial.print("D11: "); Serial.println(D11);
+  Serial.print("D12: "); Serial.println(D12);
+  Serial.print("D13: "); Serial.println(D13);
+  Serial.print("MISO: "); Serial.println(MISO);
+  Serial.print("MOSI: "); Serial.println(MOSI);
+  Serial.print("SCK: "); Serial.println(SCK);
+  Serial.print("SS: "); Serial.println(SS);
 }
 
 void loop() {
-  if (!tried) {
-    delay(5000); // Give serial monitor time to connect
-    Serial.println("\n--- Nano RP2040 Connect SD Card Test ---");
-    SPI.setRX(SD_MISO_PIN);
-    SPI.setTX(SD_MOSI_PIN);
-    SPI.setSCK(SD_SCK_PIN);
-    SPI.begin();
-    pinMode(SD_MISO_PIN, INPUT_PULLUP);
-
-    Serial.println("Initializing SD card at 1MHz...");
-    if (!sd.begin(SdSpiConfig(SD_CS_PIN, SHARED_SPI, SD_SCK_MHZ(1)))) {
-      Serial.println("Initialization failed!");
-      sd.initErrorPrint(&Serial);
-    } else {
-      Serial.println("Initialization SUCCESS!");
-      initSuccess = true;
-    }
-    tried = true;
-  }
-  
-  if (initSuccess) {
-    Serial.println("SD OK!");
-  } else {
-    Serial.println("SD FAIL!");
-  }
-  delay(2000);
+  delay(1000);
 }
+
 
 
 
