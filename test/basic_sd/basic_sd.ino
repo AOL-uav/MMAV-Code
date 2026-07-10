37889f720a6c6b2d633807ea75048ba400ad0cdb
Kai Keller <kxkeller01@example.com>
2026-07-10 13:34:09 -0400

    Auto-save (Fedora): 2026-07-10 13:34

--- test/basic_sd/basic_sd.ino
+++ test/basic_sd/basic_sd.ino
@@ -1,47 +1,47 @@
 #include <SPI.h>
 #include "SdFat.h"
 
-// In Philhower core:
-// D10 = GPIO 5 (CS)
-// D11 = GPIO 7 (MOSI)
-// D12 = GPIO 4 (MISO)
-// D13 = GPIO 6 (SCK)
-
 #define SD_CS_PIN 5
 #define SD_MOSI_PIN 7
 #define SD_MISO_PIN 4
 #define SD_SCK_PIN 6
 
 SdFat sd;
+bool initSuccess = false;
+bool tried = false;
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial && millis() < 3000);
-  
-  Serial.println("\n--- Nano RP2040 Connect SD Card Test ---");
-  
-  // Set explicit pins for Philhower core
-  SPI.setRX(SD_MISO_PIN);
-  SPI.setTX(SD_MOSI_PIN);
-  SPI.setSCK(SD_SCK_PIN);
-  SPI.begin();
-  
-  // Optional: Add pull-up to MISO
-  pinMode(SD_MISO_PIN, INPUT_PULLUP);
-
-  Serial.println("Initializing SD card at 1MHz...");
-  
-  if (!sd.begin(SdSpiConfig(SD_CS_PIN, SHARED_SPI, SD_SCK_MHZ(1)))) {
-    Serial.println("Initialization failed!");
-    sd.initErrorPrint(&Serial);
-  } else {
-    Serial.println("Initialization SUCCESS!");
-    sd.ls(&Serial, LS_R);
-  }
 }
 
 void loop() {
-  delay(1000);
+  if (!tried) {
+    delay(5000); // Give serial monitor time to connect
+    Serial.println("\n--- Nano RP2040 Connect SD Card Test ---");
+    SPI.setRX(SD_MISO_PIN);
+    SPI.setTX(SD_MOSI_PIN);
+    SPI.setSCK(SD_SCK_PIN);
+    SPI.begin();
+    pinMode(SD_MISO_PIN, INPUT_PULLUP);
+
+    Serial.println("Initializing SD card at 1MHz...");
+    if (!sd.begin(SdSpiConfig(SD_CS_PIN, SHARED_SPI, SD_SCK_MHZ(1)))) {
+      Serial.println("Initialization failed!");
+      sd.initErrorPrint(&Serial);
+    } else {
+      Serial.println("Initialization SUCCESS!");
+      initSuccess = true;
+    }
+    tried = true;
+  }
+  
+  if (initSuccess) {
+    Serial.println("SD OK!");
+  } else {
+    Serial.println("SD FAIL!");
+  }
+  delay(2000);
 }
+
 
 
