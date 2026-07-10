ddd90e3cd0dfebd0ca4cffc14cb723e2faac07e7
Kai Keller <kxkeller01@example.com>
2026-07-10 13:03:03 -0400

    Auto-save (Fedora): 2026-07-10 13:03

--- test/basic_sd/basic_sd.ino
+++ test/basic_sd/basic_sd.ino
@@ -1,50 +1,218 @@
-#include <Arduino.h>
-#include <SPI.h>
+/****************************************************************************************************************************
+  CardInfo.ino
+  
+  For all RP2040 boads using Arduimo-mbed or arduino-pico core
+  
+  RP2040_SD is a library enable the usage of SD on RP2040-based boards
+  
+  This Library is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-void setup() {
-  Serial.begin(115200);
+  This Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with the Arduino SdFat Library.  
+  If not, see <http://www.gnu.org/licenses/>.
+  
+  Based on and modified from  Arduino SdFat Library (https://github.com/arduino/Arduino)
+  
+  (C) Copyright 2009 by William Greiman
+  (C) Copyright 2010 SparkFun Electronics
+  (C) Copyright 2021 by Khoi Hoang
+  
+  Built by Khoi Hoang https://github.com/khoih-prog/RP2040_SD
+  Licensed under GPL-3.0 license
+ *****************************************************************************************************************************/
+/*
+  SD card connection
+
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
+   // Arduino-pico core
+   ** MISO - pin 16
+   ** MOSI - pin 19
+   ** CS   - pin 17
+   ** SCK  - pin 18
+
+   // Arduino-mbed core
+   ** MISO - pin 4
+   ** MOSI - pin 3
+   ** CS   - pin 5
+   ** SCK  - pin 2
+*/
+
+
+#if !defined(ARDUINO_ARCH_RP2040)
+  #error For RP2040 only
+#endif
+
+#if defined(ARDUINO_ARCH_MBED)
+  
+  #define PIN_SD_MOSI       PIN_SPI_MOSI
+  #define PIN_SD_MISO       PIN_SPI_MISO
+  #define PIN_SD_SCK        PIN_SPI_SCK
+  #define PIN_SD_SS         PIN_SPI_SS
+
+#else
+
+  #define PIN_SD_MOSI       PIN_SPI0_MOSI
+  #define PIN_SD_MISO       PIN_SPI0_MISO
+  #define PIN_SD_SCK        PIN_SPI0_SCK
+  #define PIN_SD_SS         PIN_SPI0_SS
+  
+#endif
+
+#define _RP2040_SD_LOGLEVEL_       1
+
+#include <SPI.h>
+#include <RP2040_SD.h>
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+File root;
+
+const int chipSelect = PIN_SD_SS;
+
+void printDirectory(File dir, int numTabs) 
+{
+  while (true) 
+  {
+    File entry =  dir.openNextFile();
+
+    if (! entry) 
+    {
+      // no more files
+      break;
+    }
+    
+    for (uint8_t i = 0; i < numTabs; i++) 
+    {
+      Serial.print('\t');
+    }
+    
+    Serial.print(entry.name());
+    
+    if (entry.isDirectory()) 
+    {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } 
+    else 
+    {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    
+    entry.close();
+  }
 }
 
-void loop() {
-  Serial.println("Raw SPI SD CMD0 Test...");
+void setup() 
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial);
+
+  delay(1000);
+
+#if defined(ARDUINO_ARCH_MBED)
+  Serial.print("Starting SD Card CardInfo on MBED ");
+#else
+  Serial.print("Starting SD Card CardInfo on ");
+#endif
   
-  pinMode(10, OUTPUT);
-  digitalWrite(10, HIGH);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(11, OUTPUT);
-  digitalWrite(11, HIGH);
-  SPI.begin();
+  Serial.println(BOARD_NAME);
+  Serial.println(RP2040_SD_VERSION);
   
-  // 80 dummy clocks with CS high to initialize SD
-  SPI.beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
-  for (int i = 0; i < 10; i++) SPI.transfer(0xFF);
-  
-  // Send CMD0 (GO_IDLE_STATE)
-  digitalWrite(10, LOW);
-  SPI.transfer(0x40 | 0); // CMD0
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
-  SPI.transfer(0x95);     // CRC for CMD0
-  
-  // Read response
-  uint8_t res = 0xFF;
-  for (int i = 0; i < 10; i++) {
-    res = SPI.transfer(0xFF);
-    if (res != 0xFF) break;
+  Serial.print("Initializing SD card with SS = ");  Serial.println(PIN_SD_SS);
+  Serial.print("SCK = ");   Serial.println(PIN_SD_SCK);
+  Serial.print("MOSI = ");  Serial.println(PIN_SD_MOSI);
+  Serial.print("MISO = ");  Serial.println(PIN_SD_MISO);
+
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) 
+  {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    while (1);
+  } 
+  else 
+  {
+    Serial.println("Wiring is correct and a card is present.");
   }
-  digitalWrite(10, HIGH);
-  SPI.endTransaction();
+
+  // print the type of card
+  Serial.println();
+  Serial.print("Card type:         ");
   
-  Serial.print("CMD0 Response: 0x");
-  Serial.println(res, HEX);
-  
-  if (res == 0x01) {
-    Serial.println("Hardware SPI is ALIVE and SD card responded!");
-  } else {
-    Serial.println("SPI failed or SD card did not respond properly.");
+  switch (card.type()) 
+  {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) 
+  {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    while (1);
+  }
+
+  Serial.print("Clusters:          ");
+  Serial.println(volume.clusterCount());
+  Serial.print("Blocks x Cluster:  ");
+  Serial.println(volume.blocksPerCluster());
+
+  Serial.print("Total Blocks:      ");
+  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
+  Serial.println();
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("Volume type is:    FAT");
+  Serial.println(volume.fatType(), DEC);
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
+  Serial.print("Volume size (Kb):  ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mb):  ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Gb):  ");
+  Serial.println((float)volumesize / 1024.0);
+
+  if (!SD.begin(PIN_SD_SS)) 
+  {
+    Serial.println("Initialization failed!");
+    return;
   }
   
-  delay(2000);
+  Serial.println("Initialization done.");
+
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+
+  root = SD.open("/");
+
+  printDirectory(root, 0);
+}
+
+void loop() 
+{
 }
