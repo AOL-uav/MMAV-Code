464f4525b5758e96c8d987ade2ce8b6c2d7638ea
Kai Keller <kxkeller01@example.com>
2026-07-10 13:44:19 -0400

    Auto-save (Fedora): 2026-07-10 13:44

--- test/basic_sd/basic_sd.ino
+++ test/basic_sd/basic_sd.ino
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include <SPI.h>
 #include "SdFat.h"
 
@@ -11,12 +10,35 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Initializing SD card...");
+  if (!tried) {
+    delay(3000);
+    Serial.println("\n--- SD Card Test (SdFs) ---");
+    SPI.setRX(4);
+    SPI.setTX(7);
+    SPI.setSCK(6);
+    SPI.begin();
+    pinMode(4, INPUT_PULLUP);
 
-  if (!sd.begin(10)) {
-    Serial.println("initialization failed!");
-  } else {
-    Serial.println("initialization done.");
+    if (!sd.begin(SdSpiConfig(5, SHARED_SPI, SD_SCK_MHZ(1)))) {
+      Serial.println("Init failed!");
+      sd.initErrorPrint(&Serial);
+    } else {
+      Serial.println("Init SUCCESS!");
+      initSuccess = true;
+      sd.ls(&Serial, LS_R);
+      
+      // Let's test writing
+      FsFile file;
+      if (file.open("test.txt", O_RDWR | O_CREAT | O_TRUNC)) {
+        file.println("Testing SD card writing!");
+        file.sync();
+        file.close();
+        Serial.println("File write successful!");
+      } else {
+        Serial.println("File write failed!");
+      }
+    }
+    tried = true;
   }
-  delay(2000);
+  delay(1000);
 }
