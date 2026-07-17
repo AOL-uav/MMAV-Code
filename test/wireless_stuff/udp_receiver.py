b58e2b172c25611d9eba05e2430cbc938c0eb797
Kai Keller <kxkeller01@example.com>
2026-07-17 14:28:56 -0400

    Auto-save (Fedora): 2026-07-17 14:28

--- test/wireless_stuff/udp_receiver.py
+++ test/wireless_stuff/udp_receiver.py
@@ -6,6 +6,8 @@ UDP_IP = "0.0.0.0" # Listen on all interfaces
 UDP_PORT = 5000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sock.bind((UDP_IP, UDP_PORT))
 sock.setblocking(False)
 
