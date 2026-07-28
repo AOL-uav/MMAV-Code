1f4f9aee3072196be5ea225372b56a080f60cdc7
Kai Keller <kxkeller01@example.com>
2026-07-28 15:31:12 -0400

    Auto-save (Fedora): 2026-07-28 15:31

--- test/rotational_mode/filterRotLed/filterRotLed.ino
+++ test/rotational_mode/filterRotLed/filterRotLed.ino
@@ -74,7 +74,7 @@
 
 // ========================= Edit each flight =========================
 
-static const char LOG_TAG[] = "0724";
+static const char LOG_TAG[] = "072";
 
 
 // ========================= Fixed test position =========================
